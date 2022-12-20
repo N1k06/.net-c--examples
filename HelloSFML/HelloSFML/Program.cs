1ea0b705eac93a8d837d89525f6caad5464bf88e
N1k06 <n.falcionelli@pm.univpm.it>
2022-12-20 08:53:56 +0100

    Update Program.cs

--- HelloSFML/HelloSFML/Program.cs
+++ HelloSFML/HelloSFML/Program.cs
@@ -1,32 +1,80 @@
-﻿using System;
-using SFML.Window;
+using System;
 using SFML.Graphics;
+using SFML.Window;
 using SFML.System;
+
 
 namespace HelloSFML
 {
     class Program
     {
+        //altezza e larghezza della finestra
         const int WIDTH = 640;
         const int HEIGHT = 480;
-        const string TITLE = "SHMUP";
+        const string TITLE = "Hello SFML!";
+
+        //creazione forma circolare
+        static CircleShape circle1 = new CircleShape(50);
+
+        static private void OnKeyPressed(object sender, KeyEventArgs e)
+        {
+            //stampo il tasto premuto
+            Console.WriteLine(e);
+
+            //prendo la posizione corrente della forma
+            Vector2f pos = circle1.Position;
+
+            //in base al tasto premuto scelgo cosa fare
+            switch (e.Code)
+            {
+                case Keyboard.Key.A:
+                    pos.X -= 10;
+                    circle1.Position = pos;
+                    break;
+                case Keyboard.Key.D:
+                    pos.X += 10;
+                    circle1.Position = pos;
+                    break;
+                case Keyboard.Key.W:
+                    pos.Y -= 10;
+                    circle1.Position = pos;
+                    break;
+                case Keyboard.Key.S:
+                    pos.Y += 10;
+                    circle1.Position = pos;
+                    break;
+            }
+        }
+
         static void Main(string[] args)
         {
+            //impostazioni finestra
             VideoMode mode = new VideoMode(WIDTH, HEIGHT);
             RenderWindow window = new RenderWindow(mode, TITLE);
-            CircleShape shape = new CircleShape(50);
-            shape.FillColor = new Color(100, 250, 50);
-
             window.SetVerticalSyncEnabled(true);
 
+            //creazione figura
+            circle1.FillColor = new Color(100, 250, 50);
+
+            //evento chiusura della finestra
             window.Closed += (sender, args) => window.Close();
 
+            //gestione eventi della tastiera
+            window.KeyPressed += OnKeyPressed;
+
+            //loop principale
             while (window.IsOpen)
             {
+                //gestione degli eventi
                 window.DispatchEvents();
 
+                //pulizia della finestra
                 window.Clear(Color.Black);
-                window.Draw(shape);
+
+                //disegna la figura
+                window.Draw(circle1);
+
+                //visualizza i contenuti disegnati
                 window.Display();
             }
         }
